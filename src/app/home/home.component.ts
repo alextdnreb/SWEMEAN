3f6e9cb0e63a331e91444c64d43aa639a2302223
alex.berndt95@web.de <alex.berndt95@web.de>
2020-05-23 17:21:57 +0200

    fixup title home component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { Title } from '@angular/platform-browser';
 
 @Component({
     selector: 'swe-home',
     templateUrl: './home.component.html',
     styles: [],
 })
-export class HomeComponent {
-    constructor() {
+export class HomeComponent implements OnInit {
+    constructor(private readonly titleService: Title) {
         console.log('HomeComponent.Constructor()');
+    }
+
+    ngOnInit() {
+        this.titleService.setTitle('Details');
     }
 }
