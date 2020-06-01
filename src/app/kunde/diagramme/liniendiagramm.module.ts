62a239fba8822dee77836752d20e8123dbe70fbd
Sebastian Heinz <heinz.sebastian@gmx.net>
2020-06-01 09:53:51 +0200

    small styling adjustments

--- src/app/kunde/diagramme/liniendiagramm.module.ts
+++ src/app/kunde/diagramme/liniendiagramm.module.ts
@@ -1,8 +1,6 @@
 /*BILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 import { HttpClientModule } from '@angular/common/http';
